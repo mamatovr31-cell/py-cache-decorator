5c1ca0036c87ca84e38d6eca20eb300679583d89
Rustam Mamatov <Mamatovr31@gmail.com>
2026-01-01 22:56:15 +0200

    Fixed mistake ANN201

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Any
 from functools import wraps
 
 
@@ -6,7 +6,7 @@ def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs -> Any:
         key = (args, tuple(sorted(kwargs.items())))
         if key in cache_dict:
             print("Getting from cache")
